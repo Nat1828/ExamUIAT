1a636129ce6c10a79ea3c432e7b7d584af727b62
NRyabokon <nryabokon30@gmail.com>
2025-03-04 22:50:50 +0100

    my exam firstTest

---
+++ ExamRyabokonUIAT/src/test/java/org/example/loginTest/LoginTestInOneClass.java
@@ -0,0 +1,43 @@
+package org.example.loginTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTestInOneClass {
+    private WebDriver webDriver;
+    private Logger logger = Logger.getLogger(getClass());
+
+    @Before
+    public void setUp () {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        logger.info("Browser was opened");
+
+    }
+
+    @After
+    public void tearDown () {
+        webDriver.quit();
+        logger.info("Browser was closed");
+
+    }
+    @Test
+    public void validLogin() {
+webDriver.get("https://isqi.org");
+logger.info("Site was opened");
+        WebElement closeModalWindow = webDriver.findElement(By.xpath("//button[@class='close sus-close']"));
+        closeModalWindow.click();
+        logger.info("Modal window is closed");
+    }
+}
